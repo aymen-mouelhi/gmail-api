bb9bfdbecffb1bd4cdc01b5c3d2e6fefb8503b90
Aymen Mouelhi <aymen.mouelhi@gmail.com>
2016-04-22 17:10:01 +0200

    format Gmail Message

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var Gmail = function(auth, credentials) {
 
 // Messages Management
 Gmail.prototype.getMessage = function(messageId, callback) {
-    // Retrun content, sender, subject, recieved on, id, snippet, attachments
+    // Retrun content, from, subject, recieved on, id, snippet, attachments
     if (messageId) {
         gmail.users.messages.get({
             auth: auth,
@@ -24,17 +24,18 @@ Gmail.prototype.getMessage = function(messageId, callback) {
             if (error) {
                 callback(error);
             } else {
+                // Get message
                 _formatMessage(message, callback);
             }
         });
     } else {
-    	callback('Message Id cannot be empty');
+        callback('Message Id cannot be empty');
     }
 };
 
 
 Gmail.prototype.getMessageAttachements = function(messageId, callback) {
-    // Retrun content, sender, subject, recieved on, id, snippet, attachments
+    // Retrun content, from, subject, recieved on, id, snippet, attachments
 };
 
 Gmail.prototype.getMessages = function(limit, fields, callback) {
@@ -84,7 +85,6 @@ Gmail.prototype.send = function(payload, callback) {
 };
 
 // Draft Management
-
 Gmail.prototype.createDraft = function(payload, callback) {
 
 };
@@ -98,3 +98,89 @@ Gmail.prototype.createDraft = function(payload, callback) {
 Gmail.prototype._createEmail = function(payload, callback) {
 
 };
+
+
+Gmail.prototype._formatMessage = function(message, callback) {
+
+    var from;
+    var subject;
+    var date;
+    var content;
+    var to;
+    var delivered_to;
+
+    // Get Headers
+    var headers = message.payload.headers;
+
+    // Check message headers
+    for (var j in headers) {
+        if (headers[j].name.indexOf('to') > -1) {
+            from = headers[j].value;
+        } else if (headers[j].name.indexOf('Subject') > -1) {
+            subject = headers[j].value;
+        } else if (headers[j].name.indexOf('Date') > -1) {
+            date = headers[j].value;
+        } else if (headers[j].name === 'To' > -1) {
+            to = headers[j].value;
+        } else if (headers[j].name.indexOf('Delivered-To') > -1) {
+            delivered_to = headers[j].value;
+        }
+
+        subject = subject || message.snippet;
+    }
+
+
+    var parts = message.payload.parts;
+
+    if (!parts) {
+        parts = headers.parts;
+    }
+
+    // Store snippet in content until we find better content
+    content = message.snippet;
+
+    // Check payloead parts
+    if (message.payload.parts) {
+        content = message.payload.parts[0].body.data;
+        if (content) {
+            content = new Buffer(content, 'base64').toString('utf8');
+        }
+    }
+
+    // Check payload.data
+    if (content === message.snippet) {
+        if (message.payload.body) {
+            content = message.payload.body.data;
+            content = new Buffer(content, 'base64').toString('utf8');
+        }
+    }
+
+    if (content == '') {
+        content = message.snippet;
+    }
+
+    // Get from address
+    from = from.substr(from.indexOf('<'), from.indexOf('>')).replace('<', '').replace('>', '');
+
+    // Get Reciever
+    to = to.substr(to.indexOf('<'), to.indexOf('>')).replace('<', '').replace('>', '');
+
+    if (!to) {
+        to = delivered_to;
+    }
+
+    // Todo: Get Attachements
+
+
+    var msg = {
+        from: from,
+        to: to,
+        subject: subject,
+        content: content,
+        date: date
+        snippet: message.snippet,
+        attachements: []
+    }
+
+    callback(null, msg);
+}
